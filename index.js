8f75d17a2ae5a77d5387b02c105973badb4f8791
Suvankar <suvankarpradhan100@gmail.com>
2021-05-08 12:31:29 +0530

    desktop design complete

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const slider = document.getElementById('priceRange');
 var views = document.getElementById('view');
 var price = document.getElementById('price');
+var yearPlan = document.getElementById('yearPlan');
+yearPlan.onclick = ()=>{
+       checkRange(); 
+}
 slider.oninput = ()=>{
-    var range = slider.value;
+    checkRange();
+}
+function checkRange(){
+        var range = slider.value;
     if(range>=0 && range<20){    
             views.innerHTML = " 10K ";
-            price.innerHTML = " $8.00 ";
+            (yearPlan.checked == false)?price.innerHTML = " $8.00 ":price.innerHTML = "$"+(8.00*0.75)+".00 ";
     }else if(range>=20 && range<40){
             views.innerHTML = " 50K ";
-            price.innerHTML = " $12.00 ";
+            (yearPlan.checked == false)?price.innerHTML = " $12.00 ":price.innerHTML = "$"+(12.0*0.75)+".00 ";
     }else if(range>=40 && range<60){
             views.innerHTML = " 100K ";
-            price.innerHTML = " $16.00 ";
+            (yearPlan.checked == false)?price.innerHTML = " $16.00 ":price.innerHTML = "$"+(16.0*0.75)+".00 ";
     }else if(range>=60 && range<80){
             views.innerHTML = " 500k ";
-            price.innerHTML = " $24.00 ";
+            (yearPlan.checked == false)?price.innerHTML = " $24.00 ":price.innerHTML = "$"+(24.0*0.75)+".00 ";
     }else{
             views.innerHTML = " 1M ";
-            price.innerHTML = " $36.00 ";
-            
+            (yearPlan.checked == false)?price.innerHTML = " $36.00 ":price.innerHTML = "$"+(36.00*0.75)+".00 ";            
     }
     slider.style.background = 'linear-gradient(90deg,hsl(174, 77%, 80%) '+range+'%,hsl(224, 65%, 95%) '+(range-slider.min)+'%)';
 }
